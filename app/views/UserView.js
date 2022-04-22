375b5fee915b792500373ce8f1f70cec6ebf8bae
HenryJimenezP <ing.henry.jimenez.95@hotmail.com>
2022-04-22 15:54:17 -0700

    Agregando requerimiento 1

---
+++ app/views/UserView.js
@@ -0,0 +1,10 @@
+const UserService = require ('./../services/UserService')
+class UserView{
+    static createUser(payload){
+        if (payload == null){
+            return{error: "payload no existe"}
+        }
+    }
+}
+
+module.exports = UserView
