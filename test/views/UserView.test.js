8b9f35adee293021bcda8445c631f6917d337644
HenryJimenezP <ing.henry.jimenez.95@hotmail.com>
2022-04-23 14:07:03 -0700

    Agregando requerimiento 3 - Sprint 3

--- test/views/UserView.test.js
+++ test/views/UserView.test.js
@@ -11,4 +11,9 @@ describe("Tests for UserView", () => {
         const result = UserView.createUser(payload)
         expect(result.error).toMatch(/necesitan tener un valor válido/)
     })
+    test("Return an error object when try to create a new user with a payload with invalid properties", () => {
+        const payload = {username: "Usernama"}
+        const result = UserView.createUser(payload)
+        expect(result.error).toMatch(/necesitan tener un valor válido/)
+    })
 })
