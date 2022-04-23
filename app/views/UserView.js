b06ddb925196a1dedd1abb2494965dfb4bfc0939
HenryJimenezP <ing.henry.jimenez.95@hotmail.com>
2022-04-23 13:59:42 -0700

    Agregando requerimiento 2 - Sprint 3

--- app/views/UserView.js
+++ app/views/UserView.js
@@ -1,8 +1,13 @@
 const UserService = require ('./../services/UserService')
 class UserView{
     static createUser(payload){
-        if (payload == null){
+        if (payload === null){
             return{error: "payload no existe"}
+        }else
+        if(typeof payload.username === 'string' && typeof payload.name === 'string' && typeof payload.id === 'number' ){
+            return UserService.create(payload.username, payload.name, payload.id)
+        }else{
+            return{error: "Las propiedades necesitan tener un valor válido"}
         }
     }
 }
